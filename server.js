00d1c2da4cae11239b409b5e0f51719b18aacbfe
zedbpro <zedblqpam@gmail.com>
2019-02-11 19:03:32 +0100

    1

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var taha = express();
+
+taha.get('/',function(req,res){
+
+/*res.send('taha is the best');*/
+res.sendFile(__dirname + '/index.html');
+
+});
+taha.listen(3000);
+
+
+
+
+
+
+
+
+
+/*console.log('server is strting');
+
+var express = require('express');
+var app = express();
+var server =listen(3000, listening);
+
+function listening() {
+
+
+  console.log("listening...");
+}
+*/
